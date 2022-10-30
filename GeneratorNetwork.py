23868ec158c2821517fa68be4a6151e454bd3055
tavoduv <tavaoduv@umich.edu>
2022-10-30 17:12:28 +0000

    finished genenetwork, made into class

--- GeneratorNetwork.py
+++ GeneratorNetwork.py
@@ -1,93 +1,47 @@
-<<<<<<< HEAD
 import tensorflow as tf
 from keras import Input
 from keras.applications import VGG19
-#from keras.callbacks import TensorBoard
+# from keras.callbacks import TensorBoard
 from keras.layers import BatchNormalization, Activation, PReLU, LeakyReLU, Add, Dense
 from keras.layers.convolutional import Conv2D, UpSampling2D
-#from tensorflow.keras.backend import set_session
-from tensorflow.keras.models import load_model
 
-lowres_h, lowres_w = 52,52
-nresidual = 10
 
-inputs = (52, 52, 3)
-conv = Conv2D(32, (3,3), input_shape=(lowres_h,lowres_w,3))
-cur = conv(inputs)
-cur = PReLU(shared_axes=[1,2])(cur)
-skip = cur
-def addResidualBlock(x,kernel_size,filters):
-    a=x#takes cur=x and adds the residual block
-    x = Conv2D(kernel_size=kernel_size,filters=filters)(x) #not sure about parameters here
-    x = BatchNormalization()(x)
-    x = PReLU(shared_axes=[1,2])(x)
+# from tensorflow.keras.backend import set_session
+def addResidualBlock(x, kernel_size, filters):
+    a = x  # takes cur=x and adds the residual block
     x = Conv2D(kernel_size=kernel_size, filters=filters)(x)
     x = BatchNormalization()(x)
-    x = Add()([a,x])
-    return x
-
-kernalsize = 420
-filters = 420 #placeholders
-#add all residual blocks
-for i in range(nresidual):
-    addResidualBlock(cur,kernalsize,filters)
-
-#add conv, BN,
-cur = Conv2D(64,(3,3))(cur)
-cur = BatchNormalization()(cur)
-cur = Add()([skip,cur])
-#Add x2 Conv,PixelshuffleX2, PReLU
-
-
-
-
-
-
-
-=======
-import tensorflow as tf
-from keras import Input
-from keras.applications import VGG19
-#from keras.callbacks import TensorBoard
-from keras.layers import BatchNormalization, Activation, PReLU, LeakyReLU, Add, Dense
-from keras.layers.convolutional import Conv2D, UpSampling2D
-#from tensorflow.keras.backend import set_session
-from tensorflow.keras.models import load_model
-
-lowres_h, lowres_w = 52,52
-nresidual = 10
-
-inputs = (52, 52, 3)
-conv = Conv2D(32, (3,3), input_shape=(lowres_h,lowres_w,3))
-cur = conv(inputs)
-cur = PReLU(shared_axes=[1,2])(cur)
-skip = cur
-def addResidualBlock(x,kernel_size,filters):
-    a=x#takes cur=x and adds the residual block
-    x = Conv2D(kernel_size=kernel_size,filters=filters)(x) #not sure about parameters here
-    x = BatchNormalization()(x)
-    x = PReLU(shared_axes=[1,2])(x)
+    x = PReLU(shared_axes=[1, 2])(x)
     x = Conv2D(kernel_size=kernel_size, filters=filters)(x)
     x = BatchNormalization()(x)
-    x = Add()([a,x])
+    x = Add()([a, x])
     return x
 
-kernalsize = 420
-filters = 420 #placeholders
-#add all residual blocks
-for i in range(nresidual):
-    addResidualBlock(cur,kernalsize,filters)
 
-#add conv, BN,
-cur = Conv2D(64,(3,3))(cur)
-cur = BatchNormalization()(cur)
-cur = Add()([skip,cur])
-#Add x2 Conv,PixelshuffleX2, PReLU
+class GenNet:
+    def __init__(self, x):
+        lowres_h, lowres_w = 52, 52
+        self.build_model(x, lowres_h, lowres_w)
 
-
-
-
-
-
-
->>>>>>> refs/remotes/origin/280p
+    def build_model(self, x, h, w):
+        nresidual = x
+        inputs = Input(shape=(h, w, 3))
+        cur = Conv2D(64, 9)(inputs)
+        cur = PReLU(shared_axes=[1, 2])(cur)
+        skip = cur
+        kernalsize = 3
+        filters = 64  # values taken from ResNet paper
+        # add all residual blocks
+        for i in range(nresidual):
+            cur = addResidualBlock(cur, kernalsize, filters)
+        # add conv, BN,
+        cur = Conv2D(64, 3)(cur)
+        cur = BatchNormalization()(cur)
+        cur = Add()([skip, cur])
+        # Add x2 Conv,PixelshuffleX2, PReLU
+        for i in range(2):
+            cur = Conv2D(256, 3)(cur)
+            cur = UpSampling2D(2)(cur)  # pixelshuffle replacement?
+            cur = PReLU(cur)
+        cur = Conv2D(3, 9)(cur)
+        return cur
